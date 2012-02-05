5dfa844d255bd6f252f61547f67632599967d9d6
Ed Shadi <edshadi@gmail.com>
2012-02-05 14:51:42 -0800

    edit and destroy functions for wikis, wiki_topics and wiki_pages

--- app/controllers/wiki_pages_controller.rb
+++ app/controllers/wiki_pages_controller.rb
@@ -4,17 +4,25 @@ class WikiPagesController < ApplicationController
   end
 
   def new
+    @wiki_page = WikiPage.new
   end
 
   def edit
+    @wiki_page = WikiPage.find params[:id]
   end
 
   def create
+    @wiki_page = WikiPage.new params[:wiki_page]
+    @wiki_page.save ? redirect_to(wiki_page_path(@wiki_page)) : render(:action => :new)
   end
 
   def update
+    @wiki_page = WikiPage.find params[:id]
+    @wiki_page.update_attributes(params[:wiki_page]) ? redirect_to(wiki_page_path(@wiki_page)) : render(:action => :edit)
   end
 
   def destroy
+    @wiki_page = WikiPage.find params[:id]
+    @wiki_page.destroy ? redirect_to(WikiPage.first) : redirect_to(@wiki_page)
   end
 end
--- app/controllers/wiki_topics_controller.rb
+++ app/controllers/wiki_topics_controller.rb
@@ -4,17 +4,25 @@ class WikiTopicsController < ApplicationController
   end
 
   def new
+    @wiki_topic = WikiTopic.new
   end
 
   def edit
+    @wiki_topic = WikiTopic.find params[:id]
   end
 
   def create
+    @wiki_topic = WikiTopic.new params[:wiki_topic]
+    @wiki_topic.save ? redirect_to(wiki_topic_path(@wiki_topic)) : render(:action => :new)
   end
 
   def update
+    @wiki_topic = WikiTopic.find params[:id]
+    @wiki_topic.update_attributes(params[:wiki_topic]) ? redirect_to(wiki_topic_path(@wiki_topic)) : render(:action => :edit)
   end
 
   def destroy
+    @wiki_topic = WikiTopic.find params[:id]
+    @wiki_topic.destroy ? redirect_to(WikiTopic.first) : redirect_to(@wiki_topic)
   end
 end
