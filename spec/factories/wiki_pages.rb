04e733891eb64ea94c3801de6a79dbc22f6932bc
Ed Shadi <edshadi@gmail.com>
2012-02-04 16:23:36 -0800

    modified layout to include topics in sidebar and previews to its right.

--- spec/factories/wiki_pages.rb
+++ spec/factories/wiki_pages.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :wiki_page do
-      content Faker::Lorem::paragraphs(10)
+      title   Faker::Lorem::words(2).join(" ")
+      content Faker::Lorem::paragraphs(10).join("\n")
       active true
     end
 end
